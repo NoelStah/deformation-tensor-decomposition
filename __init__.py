88524a228dfba496549aae414b97c52b79533765
NoelStah <n_stahlberg@gmx.de>
2025-01-10 15:48:26 +0100

    started the setup of the deformation tensor tools

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .deformation_tensor_tools import calculate_dimension, rate_of_strain_tensor, rate_of_strain_tensor_principle_axes, rate_of_strain_tensor_volumetric_part, rate_of_strain_tensor_shear_part, rotation_tensor, principle_axis_of_rotation, complete_decomposition
